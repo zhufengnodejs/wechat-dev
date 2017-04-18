cd5d7bcedfdd5f6ceafdf37cc638b68bfb24662c
zhufengnodejs <zhufengnodejs@126.com>
2017-04-18 19:59:32 +0800

    init

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 let express = require('express');
+let request = require('request');
+function getJSON(url, callback) {
+  request(url, function (err, response, body) {
+    callback(err, JSON.parse(body));
+  });
+}
 let path = require('path');
 let app = express();
+app.set('view engine','ejs');
+app.set('views',path.resolve('views'));
 let APPID = 'wx4bc04730ea7aa5ca';
 let APPSECRET = '1cbfbc33bf3af7aced8e89ea06dcdb98';
-app.get('/',function (req,res) {
+app.get('/', function (req, res) {
   res.sendFile(path.resolve('index.html'));
 });
 //一定是个公网地址，可以使用IP
-let REDIRECT_URI = 'http://123.57.223.74/callback';
-app.get('/login',function (req,res) {
+let REDIRECT_URI = encodeURIComponent('http://123.57.223.74/callback');
+app.get('/login', function (req, res) {
   let authUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`;
   res.redirect(authUrl);
 });
-app.get('/callback',function (req,res) {
-
+app.get('/callback', function (req, res) {
+  let {code} = req.query;
+  let accessUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${APPID}&secret=${APPSECRET}&code=${code}&grant_type=authorization_code`;
+  getJSON(accessUrl, function (err, data) {
+    let {access_token, openid} = data;
+    console.log(access_token, openid);
+    let userUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
+    getJSON(userUrl,function(err,user){
+        res.render('user',{user});
+    })
+  });
 });
 app.listen(80);
+
+
+
